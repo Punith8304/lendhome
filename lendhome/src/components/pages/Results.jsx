aa444e92743b7de2360ff6768e9b986921ed4e35
Punith8304 <punithkumar8304@gmail.com>
2024-10-23 06:40:06 +0530

    windows commit first

--- lendhome/src/components/pages/Results.jsx
+++ lendhome/src/components/pages/Results.jsx
@@ -10,17 +10,17 @@ const resultStyle = {
     borderRight: "solid 1px black"
 }
 function propertyCard() {
-    return <div style={{ border: "1px solid black" }}>
-        <div style={{marginLeft: "1.5rem"}}>
-            <h3>
+    return <div style={{ border: "1px solid black", width: "60vw", marginLeft: "1.5rem", width: "fit-content", height: "fit-content" }}>
+        <div style={{margin: "1.5rem"}}>
+            <h3 style={{paddingBottom: "1rem"}}>
                 <a href="/result?house=2bhk">2BHK House for rent in Gachibowli</a>
             </h3>
             <p style={{fontSize: "15px"}}>
                 Independent House, TNGOS colony
             </p>
         </div>
-        <div style={{ display: "flex", border: "1px solid black", width: "fit-content", padding: "1.5rem 2rem", marginLeft: "1.5rem"}}>
-            <div style={{borderRight: "solid 1px black", padding: "0 5rem"}}>
+        <div style={{ display: "flex", borderTop: "1px solid black", borderBottom: "1px solid black", width: "fit-content", padding: "1.5rem 2rem"}}>
+            <div style={{borderRight: "solid 1px black", padding: "0 5.25rem"}}>
                 <p>
                     25000
                 </p>
@@ -28,7 +28,7 @@ function propertyCard() {
                     rent-negotiable
                 </p>
             </div>
-            <div style={{borderRight: "solid 1px black", padding: "0 5rem"}}>
+            <div style={{borderRight: "solid 1px black", padding: "0 5.25rem"}}>
                 <p>
                     75000
                 </p>
@@ -47,8 +47,9 @@ function propertyCard() {
 
         </div>
         <div style={{display: "flex"}}>
-            <img style={{ height: "20%", width: "20%", margin: "1.5rem 1rem 1.5rem 1.5rem" }} src="https://media.istockphoto.com/id/1255835530/photo/modern-custom-suburban-home-exterior.jpg?s=612x612&w=0&k=20&c=0Dqjm3NunXjZtWVpsUvNKg2A4rK2gMvJ-827nb4AMU4=" alt="house-img" />
-            <div className="house-card-details" style={{margin: "1.5", marginLeft: "1rem"}}>
+            <img style={{ height: "200px", width: "200px", margin: "1.5rem 1rem 1.5rem 1.5rem" }} src="https://media.istockphoto.com/id/1255835530/photo/modern-custom-suburban-home-exterior.jpg?s=612x612&w=0&k=20&c=0Dqjm3NunXjZtWVpsUvNKg2A4rK2gMvJ-827nb4AMU4=" alt="house-img" />
+            <div className="house-card-details" style={{margin: "1.5", marginBottom: "0", paddingBottom: "1.5rem"}}>
+                
                 <div style={{display: "flex", paddingBottom: "1rem", borderBottom: "1px solid black"}}>
                     <div style={resultStyle}>
                         <h4>
@@ -63,7 +64,7 @@ function propertyCard() {
                         <p>Apartment Type</p>
                     </div>
                 </div>
-                <div style={{display: "flex", paddingTop: "1rem"}}>
+                <div style={{display: "flex", paddingTop: "1rem" }}>
                     <div style={resultStyle}>
                         <h4>
                             Family
@@ -77,13 +78,15 @@ function propertyCard() {
                         <p>Available from</p>
                     </div>
                 </div>
+
             <button style={{margin: "1rem 2rem auto 4rem"}}>
                 Get Owner details
             </button>
-            <button>
+            <button style={{margin: "1rem 2rem auto 4rem"}}>
                 Add to wishlist
             </button>
             </div>
+
         </div>
 
     </div>
